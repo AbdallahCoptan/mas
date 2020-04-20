a22fe67f89146bba93e44773bfaff0cdaead0fca
abdallah ibrahim <abdallah.ibrahim@uni.lu>
2020-04-20 13:04:42 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,30 @@
+
+
+<!-- //contact -->
+<div class="copy-right">
+	<div class="container">
+		<p> &copy; 2016 Centre Avicenne Luxembourg "CAL". All Rights Reserved | Design by  <a href="/MAS/index.html" target="_blank"> MAS Technology</a></p>
+	</div>
+</div>
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //smooth scrolling -->
+</body>
+</html>
+
+<?php
+$con->close();
+?>
